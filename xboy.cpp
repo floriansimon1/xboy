019bb0b6bb4a3852d28eeeebebd73299e944a810
Florian Simon <floriansimon9@gmail.com>
2018-01-08 22:13:18 +0100

    Test that instruction

--- xboy.cpp
+++ xboy.cpp
@@ -7,19 +7,21 @@
 int main(int argc, char **argv) {
   Gameboy gameboy;
 
-  gameboy.mmu.memory[0] = 0;
+  gameboy.mmu.memory[0] = 1;
+  gameboy.mmu.memory[1] = 0;
+  gameboy.mmu.memory[2] = 100;
 
   gameboy.tick();
 
-  const bool executed = gameboy.mmu.memory[10] == 100;
+  const bool executed = gameboy.cpu.bc == 100;
 
   const std::string message = (
     executed
-    ? "The NOP instruction was executed"
-    : "The NOP instruction was not executed"
+    ? "The LD bc, d16 instruction was executed: "
+    : "The LD bc, d16 instruction was not executed: "
   );
 
-  std::cout << message << std::endl;
+  std::cout << message << gameboy.cpu.bc << std::endl;
 
   return executed ? EXIT_SUCCESS : EXIT_FAILURE;
 }
