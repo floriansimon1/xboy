9c2005a0de8073a41d6766b96facdb2b4d5a7f74
Florian Simon <floriansimon9@gmail.com>
2018-01-08 23:49:24 +0100

    Test our new instruction

--- xboy.cpp
+++ xboy.cpp
@@ -7,21 +7,21 @@
 int main(int argc, char **argv) {
   Gameboy gameboy;
 
-  gameboy.mmu.memory[0] = 1;
-  gameboy.mmu.memory[1] = 0;
-  gameboy.mmu.memory[2] = 100;
+  gameboy.cpu.af        = 0b100000000;
+  gameboy.cpu.bc        = 1;
+  gameboy.mmu.memory[0] = 2;
 
   gameboy.tick();
 
-  const bool executed = gameboy.cpu.bc == 100;
+  const bool executed = gameboy.mmu.memory[1] == 1;
 
   const std::string message = (
     executed
-    ? "The LD bc, d16 instruction was executed: "
-    : "The LD bc, d16 instruction was not executed: "
+    ? "The instruction was executed: "
+    : "The instruction was not executed: "
   );
 
-  std::cout << message << gameboy.cpu.bc << std::endl;
+  std::cout << message << (unsigned int) gameboy.mmu.memory[1] << std::endl;
 
   return executed ? EXIT_SUCCESS : EXIT_FAILURE;
 }
