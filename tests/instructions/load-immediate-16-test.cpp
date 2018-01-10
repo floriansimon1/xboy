42f7728557b35cc370bf129e63d9c7f8fce2af4c
Florian Simon <florian@tentwentyfour.lu>
2018-01-10 19:49:09 +0100

    Add LoadImmediate16 tests

---
+++ tests/instructions/load-immediate-16-test.cpp
@@ -0,0 +1,22 @@
+#include "../../gameboy.hpp"
+#include "load-immediate-16-test.hpp"
+#include "../../cpu/instructions/load-immediate-16.hpp"
+
+LoadImmediate16Test::LoadImmediate16Test():
+  Test("Load immediate 16 instruction")
+{
+}
+
+bool LoadImmediate16Test::run() {
+  Gameboy         gameboy;
+  LoadImmediate16 instruction(&Cpu::bc);
+
+  const uint8_t lowByte  = 1;
+  const uint8_t highByte = 2;
+
+  uint16_t data = (highByte << 8) | lowByte;
+
+  instruction.execute(gameboy, reinterpret_cast<uint8_t*>(&data));
+
+  return gameboy.cpu.bc == data;
+}
---
+++ tests/instructions/load-immediate-16-test.hpp
@@ -0,0 +1,12 @@
+#ifndef LOAD_IMMEDIATE_16_TEST_HPP
+#define LOAD_IMMEDIATE_16_TEST_HPP
+
+#include "../test.hpp"
+
+struct LoadImmediate16Test: Test {
+  LoadImmediate16Test();
+
+  bool run() override;
+};
+
+#endif
