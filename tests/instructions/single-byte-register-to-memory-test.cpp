04e84b4d9e66b081c753ddeafae82a703ebf1fcf
Florian Simon <florian@tentwentyfour.lu>
2018-01-11 19:11:46 +0100

    Write SingleByteRegisterToMemoryTest

--- tests/instructions/single-byte-register-to-memory-test.cpp
+++ tests/instructions/single-byte-register-to-memory-test.cpp
@@ -1,6 +1,6 @@
+#include "../../cpu/instructions/single-byte-register-to-memory.hpp"
 #include "single-byte-register-to-memory-test.hpp"
 #include "../../gameboy.hpp"
-#include "../../bit.hpp"
 
 SingleByteRegisterToMemoryTest::SingleByteRegisterToMemoryTest():
   Test("Single-byte register to memory instruction")
@@ -8,5 +8,15 @@ SingleByteRegisterToMemoryTest::SingleByteRegisterToMemoryTest():
 }
 
 bool SingleByteRegisterToMemoryTest::run() {
-  return true;
+  Gameboy                    gameboy;
+  SingleByteRegisterToMemory instruction(&Cpu::bc, &Cpu::af, false);
+
+  gameboy.mmu.memory[0] = 0;
+  gameboy.cpu.bc        = 0;
+
+  gameboy.cpu.setSingleByteRegister(&Cpu::af, false, 1);
+
+  instruction.execute(gameboy, gameboy.mmu.memory);
+
+  return gameboy.mmu.memory[0] == 1;
 }
