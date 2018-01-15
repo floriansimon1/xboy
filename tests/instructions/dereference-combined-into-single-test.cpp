bc3feb47d5610c5dcc8509fb97f416f37843bfd2
Florian Simon <florian@tentwentyfour.lu>
2018-01-15 20:02:54 +0100

    Write the test for dereferencing combined into registers into a single register

--- tests/instructions/dereference-combined-into-single-test.cpp
+++ tests/instructions/dereference-combined-into-single-test.cpp
@@ -1,4 +1,6 @@
+#include "../../cpu/instructions/dereference-combined-into-single.hpp"
 #include "dereference-combined-into-single-test.hpp"
+#include "../../gameboy.hpp"
 
 DereferenceCombinedIntoSingleTest::DereferenceCombinedIntoSingleTest():
   Test("Derefence combined registers into single-byte register instruction")
@@ -6,5 +8,17 @@ DereferenceCombinedIntoSingleTest::DereferenceCombinedIntoSingleTest():
 }
 
 bool DereferenceCombinedIntoSingleTest::run() {
-  return false;
+  Gameboy                       gameboy;
+  DereferenceCombinedIntoSingle instruction(&Cpu::bc, &Cpu::af, true);
+
+  const auto value = 150;
+
+  gameboy.cpu.bc = 10;
+  gameboy.cpu.af = 0;
+
+  gameboy.mmu.memory[gameboy.cpu.bc] = value;
+
+  instruction.execute(gameboy, gameboy.mmu.memory);
+
+  return gameboy.cpu.af == value;
 }
