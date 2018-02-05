eb1e1b4cc59700f6a6cb98b8b31441fe61f51fe0
Florian Simon <florian@tentwentyfour.lu>
2018-02-05 15:55:30 +0100

    Add functions to manipulate the stack in the MMU

--- memory/mmu.cpp
+++ memory/mmu.cpp
@@ -3,9 +3,37 @@
 
 #include "mmu.hpp"
 #include "../bios.hpp"
+#include "../cpu/cpu.hpp"
 
 void Mmu::reset() {
   memset(memory, 0, sizeof(memory));
   memcpy(memory, bios, sizeof(bios));
 }
 
+uint16_t Mmu::popTwoBytesFromStack(Cpu &cpu) {
+  const auto value = *reinterpret_cast<uint16_t*>(memory + cpu.sp);
+
+  cpu.sp += 2;
+
+  return value;
+}
+
+uint8_t Mmu::popByteFromStack(Cpu &cpu) {
+  const auto value = memory[cpu.sp];
+
+  cpu.sp++;
+
+  return value;
+}
+
+void Mmu::pushTwoBytesToStack(Cpu &cpu, uint16_t value) {
+  cpu.sp -= 2;
+
+  *reinterpret_cast<uint16_t*>(memory + cpu.sp) = value;
+}
+
+void Mmu::pushByteToStack(Cpu &cpu, uint8_t value) {
+  memory[cpu.sp] = value;
+
+  cpu.sp--;
+}
--- memory/mmu.hpp
+++ memory/mmu.hpp
@@ -3,11 +3,17 @@
 
 #include <cstdint>
 
+struct Cpu;
+
 struct Mmu {
   uint8_t memory[65536];
+
+  void pushTwoBytesToStack(Cpu &cpu, uint16_t value);
+  void pushByteToStack(Cpu &cpu, uint8_t value);
+  uint16_t popTwoBytesFromStack(Cpu &cpu);
+  uint8_t popByteFromStack(Cpu &cpu);
 
   void reset();
 };
 
 #endif
-
