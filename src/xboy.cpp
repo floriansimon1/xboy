1f64b6d2ac4276e6ca762b1047c6bf7795458d7b
Florian Simon <floriansimon9@gmail.com>
2018-03-19 23:05:09 +0100

    Simple test code for the frame display

--- src/xboy.cpp
+++ src/xboy.cpp
@@ -1,6 +1,38 @@
-#include <iostream>
 #include <cstdlib>
+#include <unistd.h>
+
+#include "gui.hpp"
 
 int main(int, char **) {
+  Gui       gui;
+  sf::Event event;
+
+  while (gui.window.isOpen()) {
+    Frame frame;
+
+    while (gui.window.pollEvent(event)) {
+      if (event.type == sf::Event::Closed) {
+        gui.window.close();
+      }
+    }
+
+    for (auto i = 0; i < screenWidth; i++) {
+      for (auto j = 0; j < screenHeight; j++) {
+        const auto pixel = (i + screenWidth * j);
+
+        const uint8_t color = pixel % 256;
+
+        frame[pixel * 4]     = color;
+        frame[pixel * 4 + 1] = color;
+        frame[pixel * 4 + 2] = color;
+        frame[pixel * 4 + 3] = 255;
+      }
+    }
+
+    gui.display(frame);
+
+    sleep(1);
+  }
+
   return EXIT_SUCCESS;
 }
