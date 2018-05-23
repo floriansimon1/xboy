e906524548725edc987b9f246b29097ebdf2c645
Florian Simon <floriansimon9@gmail.com>
2018-05-23 23:25:34 +0200

    Crash if the ROM file cannot be found

--- src/gameboy/cartridge.cpp
+++ src/gameboy/cartridge.cpp
@@ -1,4 +1,5 @@
 #include <fstream>
+#include <iostream>
 
 #include "cartridge.hpp"
 
@@ -7,7 +8,16 @@ constexpr auto maxRomSize = 0x200000;
 Cartridge::Cartridge(const std::string path) {
   std::ifstream file(path, std::ios::binary);
 
+  if (!file) {
+    std::cout << path << std::endl;
+
+    throw "Cannot open " + path;
+  }
+
   const size_t fileSize = file.rdbuf()->pubseekoff(0, file.end, file.in);
+
+  // Go back to the beginning of the file
+  file.seekg(std::ios::beg);
 
   const auto romSize = maxRomSize < fileSize ? maxRomSize : fileSize;
 
